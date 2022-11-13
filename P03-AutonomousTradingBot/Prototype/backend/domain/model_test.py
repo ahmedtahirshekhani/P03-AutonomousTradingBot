e1c39764e28e4bebe4cf38117efd4f5e92e7eeea
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-11-14 02:21:41 +0500

    wrote all test cases for authentication use cases

--- P03-AutonomousTradingBot/Prototype/backend/domain/model_test.py
+++ P03-AutonomousTradingBot/Prototype/backend/domain/model_test.py
@@ -2,6 +2,7 @@ import pytest
 from uuid import uuid4
 from datetime import datetime, timedelta
 from .model import Analyst, Investor, LoginReturn
+from hashlib import sha256
 
 # Helper functions
 def create_analyst():
@@ -12,6 +13,23 @@ def create_analyst():
         email="ahmed@lums.com",
         phone_number="+92 333 3464655",
         password="832598e496ec310643ff62380fadf9a7d4d91ebf59da4b39eb86af7628c53ec6",  # SHA256 hash of password
+    )
+
+
+def create_investor(
+    username: str,
+    password: str,
+    name: str,
+    address: str,
+    phone_number: str,
+):
+    return Investor(
+        id=str(uuid4()),
+        name=name,
+        address=address,
+        email=username,
+        phone_number=phone_number,
+        password=sha256(password).hexdigest(),  # SHA256 hash of password
     )
 
 
@@ -50,6 +68,122 @@ def test_analyst_login():
     )
     assert fail_attempt.success == False
     assert fail_attempt.message == "User failed to log in!"
-    assert fail_attempt.expiry == datetime.now() + timedelta(hours=1)
-    with pytest.raises(ValueError):
-        uuid4(fail_attempt.token)  # Token will be empty on failed log in
+    assert fail_attempt.expiry == None
+    assert fail_attempt.token == None
+
+
+def test_analyst_logout():
+    new_analyst = create_analyst()
+
+    success_attempt: LoginReturn = new_analyst.login(
+        email="ahmed@lums.com",
+        password="832598e496ec310643ff62380fadf9a7d4d91ebf59da4b39eb86af7628c53ec6",
+    )
+    assert success_attempt.success == True
+    assert success_attempt.message == "User successfully logged in!"
+    assert success_attempt.expiry == datetime.now() + timedelta(hours=1)
+    try:
+        uuid4(success_attempt.token)  # This should not raise an exception
+    except:
+        pytest.fail("Raised an exception due to invalid token")
+
+    try:
+        new_analyst.logout()
+    except:
+        pytest.fail("Raised an exception due to an error in logout")
+
+
+def test_register_investor():
+    new_analyst = create_analyst()
+    invester_credentials = new_analyst.register_investor()
+
+    assert invester_credentials.success == True
+    assert invester_credentials.message == "Investor successfully registered!"
+
+    assert type(invester_credentials.username) is str
+    assert len(invester_credentials.username) >= 8
+
+    assert type(invester_credentials.password) is str
+    assert len(invester_credentials.password) >= 8
+
+
+def test_get_investor_credentials():
+    new_analyst = create_analyst()
+    invester_credentials = new_analyst.register_investor()
+
+    username = invester_credentials.username
+    password = invester_credentials.password
+
+    fetched_credentials = new_analyst.get_credentials(username)
+
+    assert fetched_credentials.password == password
+
+
+def test_investor_login():
+    new_analyst = create_analyst()
+    invester_credentials = new_analyst.register_investor()
+
+    username = invester_credentials.username
+    password = invester_credentials.password
+
+    new_investor = create_investor(
+        username,
+        password,
+        "NIC",
+        "SSE basement obviously",
+        "+92 333 3455488",
+    )
+
+    success_attempt: LoginReturn = new_investor.login(
+        email=username,
+        password=password,
+    )
+    assert success_attempt.success == True
+    assert success_attempt.message == "User successfully logged in!"
+    assert success_attempt.expiry == datetime.now() + timedelta(hours=1)
+    try:
+        uuid4(success_attempt.token)  # This should not raise an exception
+    except:
+        pytest.fail("Raised an exception due to invalid token")
+
+    fail_attempt = new_investor.login(
+        email="ahmed@lums.com",
+        password="5151sda5s1d5as1d5asd",
+    )
+    assert fail_attempt.success == False
+    assert fail_attempt.message == "User failed to log in!"
+    assert fail_attempt.expiry == None
+    assert fail_attempt.token == None
+
+
+def test_investor_logout():
+    new_analyst = create_analyst()
+    invester_credentials = new_analyst.register_investor()
+
+    username = invester_credentials.username
+    password = invester_credentials.password
+
+    new_investor = create_investor(
+        username,
+        password,
+        "NIC",
+        "SSE basement obviously",
+        "+92 333 3455488",
+    )
+
+    success_attempt: LoginReturn = new_investor.login(
+        email="ahmed@lums.com",
+        password="832598e496ec310643ff62380fadf9a7d4d91ebf59da4b39eb86af7628c53ec6",
+    )
+    assert success_attempt.success == True
+    assert success_attempt.message == "User successfully logged in!"
+    assert success_attempt.expiry == datetime.now() + timedelta(hours=1)
+    try:
+        uuid4(success_attempt.token)  # This should not raise an exception
+    except:
+        pytest.fail("Raised an exception due to invalid token")
+
+    try:
+        new_investor.logout()
+    except:
+        pytest.fail("Raised an exception due to an error in logout")
