7b73a6fc990801c601aab08bae8a508931c3ab82
Ahmed Tahir Shekhani <23100197@lums.edu.pk>
2022-11-17 21:21:15 +0500

    login integration

--- P03-AutonomousTradingBot/Prototype/frontend/next.config.js
+++ P03-AutonomousTradingBot/Prototype/frontend/next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5000/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
--- P03-AutonomousTradingBot/Prototype/frontend/pages/primary/login/services/auth.js
+++ P03-AutonomousTradingBot/Prototype/frontend/pages/primary/login/services/auth.js
@@ -1,15 +1,52 @@
-// import bcrypt from 'bcrypt'
-// const saltRounds = bcrypt.genSaltSync(10);
+import axios from 'axios';
+const apiproxy = "http://127.0.0.1:5000"
+const analystLogin = (email, password) => {
+    return new Promise((resolve, reject) => {
+        // request content type json
+        const req = {
+            "email": email,
+            "password": password
+        }
+        // convert req to json
+        const jsonReq = JSON.stringify(req);
+        axios.post('/api/analyst-login', jsonReq, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                console.log(res);
+                resolve(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+                reject(err);
+            });
+    });
+};
 
-// const hashPassword = (password) => {
-//     return bcrypt.hashSync(password, saltRounds);
-// }
+const investorLogin = (email, password) => {
+    return new Promise((resolve, reject) => {
+        axios.post('/api/investor/login', { email, password })
+            .then(res => { 
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
 
 
 const login = (email, password, role) => {
     email = email.toLowerCase();
-    // hashPassword = hashPassword(password);
-    // console.log(hashPassword)
+    if (role === 'analyst') {
+        return analystLogin(email, password);
+    }else if (role === 'admin') {
+        return investorLogin(email, password);
+    }else{
+        return analystLogin(email, password);
+    }
 }
 
 export {login}
