8796732324da2397cbb653603f386d6cb12c3aea
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-12-30 16:16:27 +0500

    added error message on analyst layout

--- P03-AutonomousTradingBot/Development/Sprint-1/frontend/components/layouts/AnalystLayout.tsx
+++ P03-AutonomousTradingBot/Development/Sprint-1/frontend/components/layouts/AnalystLayout.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import AnalystNavbar from '../navbars/AnalystNavbar';
 
 export interface IAnalystLayout {
@@ -9,11 +9,30 @@ export interface IAnalystLayout {
 const AnalystLayout: FC<IAnalystLayout> = ({ children }) => {
 	const router = useRouter();
 
+	const [showErrorMessage, setShowErrorMessage] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+
+	useEffect(() => {
+		setTimeout(() => {
+			setShowErrorMessage(false);
+		}, 5000);
+	}, [showErrorMessage]);
+
 	return (
 		<div>
 			<AnalystNavbar />
 
 			{children}
+
+			{showErrorMessage ? (
+				<div className='toast'>
+					<div className='alert alert-error'>
+						<div>
+							<span>{errorMessage}</span>
+						</div>
+					</div>
+				</div>
+			) : null}
 		</div>
 	);
 };
