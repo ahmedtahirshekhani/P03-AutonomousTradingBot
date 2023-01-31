e770cfc2b971746046f477d960db710177cd7256
Ali Asghar <alliasgher@gmail.com>
2023-01-31 11:28:45 +0500

    .

--- P03-AutonomousTradingBot/Development/Sprint-2/frontend/components/cards/stocks.tsx
+++ P03-AutonomousTradingBot/Development/Sprint-2/frontend/components/cards/stocks.tsx
@@ -9,8 +9,8 @@ const Stocks = ({items} : Dictionary ) => {
 
   return (
     <div>
-      <h1>Hello</h1>
-      <div>{items.results.cik}</div>
+      {/* <h1>Hello</h1>
+      <div>{items.results.cik}</div> */}
 
       <div className="card w-96 bg-base-100 shadow-xl">
   <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
@@ -19,14 +19,13 @@ const Stocks = ({items} : Dictionary ) => {
       {items.results.name}
       <div className="badge badge-secondary">{items.results.ticker}</div>
     </h2>
-    <p> Visit website to learn more. 
+    <p> Works in {items.results.sic_description} Visit website to learn more. 
     <a href={items.results.homepage_url} target="_blank">
           Here
         </a>
         
       </p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">{items.results.sic_description}</div> 
     </div>
   </div>
   </div>
